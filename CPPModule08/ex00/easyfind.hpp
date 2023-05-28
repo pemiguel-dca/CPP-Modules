a3a6d7deacf735441af6deb21af451cb6eb4317b
Pedro Miguel Batista Novo <pemiguel@c1r14s15.42porto.com>
2023-05-28 17:39:53 +0100

    finish CPP08

---
+++ CPPModule08/ex00/easyfind.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pemiguel <pemiguel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/28 15:11:02 by pemiguel          #+#    #+#             */
+/*   Updated: 2023/05/28 16:00:09 by pemiguel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+#include <iostream>
+#include <algorithm>
+
+template <typename T>
+
+/*Iterators are used to point at the memory addresses of STL containers.
+
+Operations of iterators:
+begin(): Returns the BEGINNING position of the container.
+end(): Returns the AFTER END position of the container.
+*/
+
+/*We need to especify that T::iterator is a type, therefore we use 'typename'
+Also we need to tell the compiler that iterator belongs to container type 'T', in this case vector<int>::iterator*/
+
+/*Since iterators do not have the operator overload '<<' we use the function std::distance()
+that returns the distance between two input iterators (first parameter being the first, and second parameter the last)*/
+void	easyfind(T &container, int toFind)
+{
+	try
+	{
+		typename T::iterator it = std::find(container.begin(), container.end(), toFind);
+		if (it == container.end())
+			throw std::exception();
+		std::cout << "Found " << toFind << " at index: " << std::distance(container.begin(), it) << std::endl;
+	}
+	catch(std::exception &error)
+	{
+		std::cerr << "There's no number " << toFind << " in the container" << std::endl;
+	}
+}
+
+#endif
---
+++ CPPModule08/ex00/main.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pemiguel <pemiguel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/28 15:10:34 by pemiguel          #+#    #+#             */
+/*   Updated: 2023/05/28 16:04:28 by pemiguel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+#include <vector>
+#include <list>
+
+/*
+Associative containers are a category of containers in C++ that store elements in a sorted and efficient manner based on a key.
+
+Examples:
+std::set: It stores a collection of unique keys in a sorted order. It does not allow duplicate elements.
+
+std::multiset: Similar to std::set, it stores a collection of keys in a sorted order, but allows duplicate elements.
+
+std::map: It stores a collection of key-value pairs in a sorted order based on the keys. Each key is unique, and associated with a value.
+
+std::multimap: Similar to std::map, it stores key-value pairs in a sorted order based on the keys, but allows duplicate keys and associated values.
+*/
+
+int main()
+{
+	/*vectors*/
+	std::vector<int> container;
+	for (int i = 1; i <= 128; i *= 2)
+		container.push_back(i);
+	easyfind(container, 128);
+
+	/*lists*/
+	std::list<int> intList;
+	for (int i = 1; i <= 300; i *= 3)
+		intList.push_back(i);
+	easyfind(intList, 7);
+
+	return (0);
+}
