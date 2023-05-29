8e5db5d792c8189b3b2e548f7a006071a2be2074
Pedro Miguel Batista Novo <pemiguel@c1r14s8.42porto.com>
2023-05-29 12:58:18 +0100

    CPP08 done

--- CPPModule08/ex02/main.cpp
+++ CPPModule08/ex02/main.cpp
@@ -6,19 +6,19 @@
 /*   By: pemiguel <pemiguel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 10:54:56 by pemiguel          #+#    #+#             */
-/*   Updated: 2023/05/29 12:36:25 by pemiguel         ###   ########.fr       */
+/*   Updated: 2023/05/29 12:57:49 by pemiguel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
-
+#include <list>
 int main()
 {
 	std::cout << "MutantStack with integers" << std::endl;
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
-	std::cout << "Just removed: " << mstack.top() << std::endl;
+	std::cout << "Last element: " << mstack.top() << std::endl;
 	mstack.pop();
 	std::cout << "Stack size:" << mstack.size() << std::endl;
 	mstack.push(3);
@@ -40,6 +40,35 @@ int main()
 	}
 	std::cout << "}" << std::endl;
 	std::stack<int> s(mstack);
+	std::cout << std::endl;
+
+	std::cout << "Using std::list" << std::endl;
+    std::list< int > lst;
+    lst.push_back(5);
+    lst.push_back(17);
+    std::cout << "Last element: " << lst.back() << std::endl;
+    lst.pop_back();
+    std::cout << "Stack size:" << lst.size() << std::endl;
+    lst.push_back(3);
+    lst.push_back(5);
+    lst.push_back(737);
+    lst.push_back(0);
+    std::list<int>::iterator lst_it = lst.begin();
+    std::list<int>::iterator lst_ite = lst.end();
+    std::cout << "{";
+	while (lst_it != lst_ite)
+	{
+		while (true)
+		{
+		std::cout << *lst_it;
+		if (++lst_it != lst_ite)
+			std::cout << ", ";
+		else
+			break;
+		}
+	}
+	std::cout << "}" << std::endl;
+    std::list<int> l(lst);
 
 	std::cout << std::endl;
 
@@ -47,7 +76,7 @@ int main()
 	MutantStack<char> letters;
 	letters.push('p');
 	letters.push('d');
-	std::cout << "Just removed: " << letters.top() << std::endl;
+	std::cout << "Last element: " << letters.top() << std::endl;
 	letters.pop();
 	letters.push('e');
 	letters.push('d');
